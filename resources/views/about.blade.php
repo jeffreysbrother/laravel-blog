f53553f9b1045d93f8745aa1e2b9af71c544c2a2
James Cool <jeffreysbrother@gmail.com>
2016-10-02 02:24:24 -0700

    create about page, based on the original welcome page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -38,8 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome!</div>
-                <p>this is the beginning</p>
+                <div class="title">About Me</div>
+                <p>Check it out! Some new nonsense!</p>
             </div>
         </div>
     </body>
